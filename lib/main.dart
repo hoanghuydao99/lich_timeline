3866957360d4ede8597e19d07e0dc1b1701648de
HoangDao <hoanghuydao99@gmail.com>
2021-01-15 09:28:26 +0700

    timeline-project

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('es'),
+        const Locale('en'),
+      ],
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  DateTime _selectedDate=DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    _resetSelectedDate();
+  }
+
+  void _resetSelectedDate() {
+    _selectedDate = DateTime.now().add(Duration(days: 0));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF333A47),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Calendar Timeline',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    .copyWith(color: Colors.tealAccent[100]),
+              ),
+            ),
+            CalendarTimeline(
+              showYears: true,
+              initialDate: _selectedDate,
+              firstDate: DateTime.now(),
+              lastDate: DateTime.now().add(Duration(days: 365)),
+              onDateSelected: (date) {
+                setState(() {
+                  _selectedDate = date;
+                });
+              },
+              leftMargin: 20,
+              monthColor: Colors.white70,
+              dayColor: Colors.teal[200],
+              dayNameColor: Color(0xFF333A47),
+              activeDayColor: Colors.white,
+              activeBackgroundDayColor: Colors.redAccent[100],
+              dotsColor: Color(0xFF333A47),
+              locale: 'en',
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: FlatButton(
+                color: Colors.teal[200],
+                child: Text('RESET', style: TextStyle(color: Color(0xFF333A47))),
+                onPressed: () => setState(() => _resetSelectedDate()),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(child: Text('Selected date is $_selectedDate', style: TextStyle(color: Colors.white)))
+          ],
+        ),
+      ),
+    );
+  }
+}
